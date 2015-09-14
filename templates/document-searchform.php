e9954a2f9376537265fa35c276e07a7ad5a30d5f
Kathy Darling <helgatheviking@gmail.com>
2015-09-14 13:58:52 -0400

    update the search form

--- templates/document-searchform.php
+++ templates/document-searchform.php
@@ -13,8 +13,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<form role="search" method="get" class="searchform documentate-document-search" action="<?php echo home_url( '/' ); ?>" autocomplete="off">
-	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'documentate' ); ?></label>
-	<input type="text" placeholder="<?php _e( 'Search Documents...', 'documentate' ); ?>" value="<?php echo get_search_query(); ?>" name="s" id="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'documentate' ); ?>" />
+<form role="search" method="get" class="search-form documentate-document-search" action="<?php echo home_url( '/' ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php _x( 'Search for documents', 'This string is a label for a search input that is only visible to screen readers.', 'documentate' ); ?></span>
+		<input type="text" placeholder="<?php _x( 'Search Documents...', 'this string is the placeholder on the search input', 'documentate' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_e( 'Press Enter to submit your search', 'documentate' ); ?>" />
+	</label>
 	<input type="hidden" value="document" name="post_type" />
 </form>
